72371b52584c07aaf6f9fee0d9425842d7876281
Ekaterina <nataveres@gmail.com>
2020-03-02 14:53:07 +0100

    Readme and the rest finished

---
+++ init.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+  debugger;
+  // log initial state
+  //document.getElementById("current-number").innerHTML = numbers.current;
+  //document.getElementById("increment").value = numbers.increment;
+
+  /*  window.onload = () => {
+    log.push({ initialNumbers: JSON.parse(JSON.stringify(numbers)) });
+    document.getElementById("current-number").innerHTML = numbers.current;
+  }; */
+
+  // render initial UI from state
+};
